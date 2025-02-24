f13353462de799e7319acd1dc999d0770d1bfd4a
TLazovic-CentennialCollege <tlazovic@my.centennialcollege.ca>
2025-02-24 13:01:31 -0500

    migration from angular 19 to 18, such that we can use primeNGv18. Front end starts from this commit.

---
+++ frontend/src/primeng.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { MessageModule } from 'primeng/message';
+import { SidebarModule } from 'primeng/sidebar';
+import { TableModule } from 'primeng/table';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { CardModule } from 'primeng/card';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ImageModule } from 'primeng/image';
+import { DropdownModule } from 'primeng/dropdown';
+import { FloatLabelModule } from 'primeng/floatlabel';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    // CommonModule,
+    ConfirmDialogModule,
+    ButtonModule,
+    DialogModule,
+    MessageModule,
+    SidebarModule,
+    TableModule,
+    FileUploadModule,
+    ToastModule,
+    CardModule,
+    CheckboxModule,
+    ImageModule,
+    DropdownModule,
+    FloatLabelModule,
+  ],
+  exports: [
+    ToastModule,
+    FileUploadModule,
+    ConfirmDialogModule,
+    ButtonModule,
+    DialogModule,
+    MessageModule,
+    SidebarModule,
+    TableModule,
+    CardModule,
+    CheckboxModule,
+    ImageModule,
+    DropdownModule,
+    FloatLabelModule,
+  ],
+
+  providers: [MessageService],
+})
+export class PrimengModule {}
