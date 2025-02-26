7fa23f0f5fb8bfc6650e492e53fe6f125d9a879f
Matthew Barhou <mbarhou@my.centennialcollege.ca>
2025-02-26 14:36:50 -0500

    backend: login functionality implemented

--- backend/express.js
+++ backend/express.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import userRoutes from './routes/userRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 
@@ -11,5 +13,9 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
+
+// Mount routes
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
 
 export default app;
