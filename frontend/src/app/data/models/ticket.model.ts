e2e72f790b040ba670dbec0f8ab2fd9f320889e5
Uğur Pekünsal <ugurpekunsal@gmail.com>
2025-03-05 18:42:06 -0500

    Added ticket creation frontend

--- frontend/src/app/data/models/ticket.model.ts
+++ frontend/src/app/data/models/ticket.model.ts
@@ -1,7 +1,9 @@
+import { StatusEnum } from "../enums/StatusEnum";
+
 export interface Ticket {
   id: number;
   userId: number; //user to whom this task belongs to
-  status: string; //open, missing documents(attention required), closed, etc.
+  status: StatusEnum; //open, missing documents(attention required), closed, etc.
   title: string;
   description: string;
   dateAndTimeOfCreation: string;
